462062935d256df9aa9a7dee8fc222aea35307cc
anthony then <anthonythen@Anthonys-MacBook-Pro.local>
2020-12-08 16:49:23 -0500

    adding retrieve messages to the service

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const app = express()
 const port = 3000
 
 var jsonParser = bodyParser.json()
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Add headers
 app.use(function (req, res, next) {
@@ -28,19 +29,17 @@ app.use(function (req, res, next) {
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader("Content-Type", "application/json");;
 
   // Pass to next layer of middleware
   next();
 });
 
 app.post('/sendMessages', jsonParser, (req, res) => {
-  let i = 0;
-
   Promise.all(
-    req.body.numbers.map(number => {
-      body = req.body.messageBody.replace('ADDRESS', req.body.addresses[i]);
-      i++;
+    req.body.numbers.map((number, index) => {
+      body = req.body.messageBody.replace('ADDRESS', req.body.addresses[index]);
       return twilio.messages.create({
         to: number,
         from: process.env.TWILIO_MESSAGING_SERVICE_SID,
@@ -49,10 +48,54 @@ app.post('/sendMessages', jsonParser, (req, res) => {
     })
   )
     .then(messages => {
-      console.log('Messages sent!');
+      console.log('Message sent!');
+      res.sendStatus(200);
     })
-    .catch(err => console.error(err))
-  console.log("done");
+    .catch(err => {
+      console.error(err)
+      res.sendStatus(500);
+    })
+  // console.log("done");
+})
+
+app.get('/retrieveMessages', jsonParser, (req, res) => {
+  const numbersMessaged = [];
+  const messageThreads = {};
+  const numbersReplied = [];
+  const messageRepliedThreads = {};
+  const resp = {};
+
+  //receives messageThreads
+  twilio.messages.list().then(messages => {
+    messages.filter(message => message.direction == "outbound-api").forEach(m => {
+      if (!numbersMessaged.includes(m.to)) {
+        numbersMessaged.push(m.to);
+      }
+    })
+    numbersMessaged.forEach(number => {
+      if (messageThreads[number]) {
+        messageThreads[number] = messageThreads.get(number).concat(messages.filter(message => message.from == number)).sort((messageA, messageB) => (messageA.dateSent > messageB.dateSent) ? 1 : -1);
+      } else {
+        messageThreads[number] = messages.filter(message => message.from == number || message.to == number).sort((messageA, messageB) => (messageA.dateSent > messageB.dateSent) ? 1 : -1);
+      }
+    })
+
+    messages.filter(message => message.direction == "inbound").forEach(m => {
+      if (!numbersReplied.includes(m.from)) {
+        numbersReplied.push(m.from);
+      }
+    })
+    numbersReplied.forEach(number => {
+      if (messageRepliedThreads[number]) {
+        messageRepliedThreads[number] = messageRepliedThreads.get(number).concat(messages.filter(message => message.from == number)).sort((messageA, messageB) => (messageA.dateSent > messageB.dateSent) ? 1 : -1);
+      } else {
+        messageRepliedThreads[number] = messages.filter(message => message.from == number || message.to == number).sort((messageA, messageB) => (messageA.dateSent > messageB.dateSent) ? 1 : -1);
+      }
+    })
+    resp['allMessages'] = messageThreads;
+    resp['conversations'] = messageRepliedThreads;
+    res.send(resp);
+  });
 })
 
 app.listen(port, () => {
