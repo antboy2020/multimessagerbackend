dbbfa74c058e582cb195a1e0791a23c0c9db81b3
anthony then <anthonythen@Anthonys-MBP.lan1>
2021-03-05 16:34:55 -0500

    all functionality for multimessaging ready

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-var express = require('express')
-var bodyParser = require('body-parser')
+var express = require('express');
+var bodyParser = require('body-parser');
+// const axios = require('axios');
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -8,10 +9,16 @@ const twilio = require('twilio')(
   process.env.TWILIO_AUTH_TOKEN
 );
 
-const app = express()
-const port = 3000
+// const Nexmo = require('nexmo');
+// const nexmo = new Nexmo({
+//   apiKey: '448f33c3',
+//   apiSecret: 'mLkrhgNwyh8vxZEk'
+// });
 
-var jsonParser = bodyParser.json()
+const app = express();
+const port = 3000;
+
+var jsonParser = bodyParser.json();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -36,50 +43,90 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.post('/sendMessages', jsonParser, (req, res) => {
-  Promise.all(
-    req.body.numbers.map((number, index) => {
-      body = req.body.messageBody.replace('ADDRESS', req.body.addresses[index]);
-      return twilio.messages.create({
-        to: number,
-        from: process.env.TWILIO_MESSAGING_SERVICE_SID,
-        body: body
-      });
+app.get('/getMessages', jsonParser, (req, res) => {
+  let messagesWithClient = [];
+  let from = false;
+  let to = false;
+  twilio.messages
+    .list({
+      from: req.query.clientNumber,
     })
-  )
     .then(messages => {
-      console.log('Message sent!');
+      messagesWithClient.push.apply(messagesWithClient, messages);
+      from = true;
+      if (to) {
+        messagesWithClient.sort((messageA, messageB) => (messageA.dateSent > messageB.dateSent) ? 1 : -1);
+        res.send(messagesWithClient);
+      }
+      console.log(messages.length);
+    });
+  twilio.messages
+    .list({
+      to: req.query.clientNumber,
+    }).then(messages => {
+      messagesWithClient.push.apply(messagesWithClient, messages);
+      to = true;
+      if (from) {
+        messagesWithClient.sort((messageA, messageB) => (messageA.dateSent > messageB.dateSent) ? 1 : -1);
+        res.send(messagesWithClient);
+      }
+      console.log(messages);
+    })
+
+})
+
+app.post('/sendMessage', jsonParser, (req, res) => {
+  twilio.messages
+    .create({ body: req.body.message, from: req.body.from, to: req.body.to })
+    .then(message => {
       res.sendStatus(200);
-    })
-    .catch(err => {
-      console.error(err)
-      res.sendStatus(500);
-    })
-  // console.log("done");
+    }).catch(err => {
+      console.log(err)
+    });
+})
+
+app.post('/sendMessages', jsonParser, (req, res) => {
+  const promises = [];
+  let j = 0;
+  for (let i = 0; i < req.body.numbers.length; i++) {
+    let body = req.body.messageBody.replace('ADDRESS', req.body.addresses[i]);
+    if (req.body.numbers[i] == req.body.numbers[i + 1]) {
+      body += " Or any of these properties";
+      for (j = 1; req.body.numbers[i] == req.body.numbers[i + j]; j++) {
+        body += " ";
+        body += req.body.addresses[i + j];
+      }
+      body += "?";
+    }
+    promises.push(twilio.messages.create({
+      to: req.body.numbers[i],
+      from: process.env.TWILIO_MESSAGING_SERVICE_SID,
+      body: body
+    }).then((success) => {
+      console.log(success);
+    }).catch((error) => {
+      console.log(error);
+    }));
+    if (j > 0) {
+      i = i + j - 1;
+    }
+    j = 0;
+  }
 })
 
 app.get('/retrieveMessages', jsonParser, (req, res) => {
+  console.log("wtf hit");
   const numbersMessaged = [];
   const messageThreads = {};
   const numbersReplied = [];
   const messageRepliedThreads = {};
   const resp = {};
 
-  //receives messageThreads
-  twilio.messages.list().then(messages => {
-    messages.filter(message => message.direction == "outbound-api").forEach(m => {
-      if (!numbersMessaged.includes(m.to)) {
-        numbersMessaged.push(m.to);
-      }
-    })
-    numbersMessaged.forEach(number => {
-      if (messageThreads[number]) {
-        messageThreads[number] = messageThreads.get(number).concat(messages.filter(message => message.from == number)).sort((messageA, messageB) => (messageA.dateSent > messageB.dateSent) ? 1 : -1);
-      } else {
-        messageThreads[number] = messages.filter(message => message.from == number || message.to == number).sort((messageA, messageB) => (messageA.dateSent > messageB.dateSent) ? 1 : -1);
-      }
-    })
+  // var oneMonthAgo = new Date();
+  // oneMonthAgo.setMonth(oneMonthAgo.getMonth - 1);
 
+  //receives messageThreads
+  twilio.messages.list({ limit: 1000 }).then(messages => {
     messages.filter(message => message.direction == "inbound").forEach(m => {
       if (!numbersReplied.includes(m.from)) {
         numbersReplied.push(m.from);
@@ -92,7 +139,21 @@ app.get('/retrieveMessages', jsonParser, (req, res) => {
         messageRepliedThreads[number] = messages.filter(message => message.from == number || message.to == number).sort((messageA, messageB) => (messageA.dateSent > messageB.dateSent) ? 1 : -1);
       }
     })
-    resp['allMessages'] = messageThreads;
+
+    messages.filter(message => message.direction == "outbound-api").forEach(m => {
+      if (!numbersMessaged.includes(m.to) && !numbersReplied.includes(m.to)) {
+        numbersMessaged.push(m.to);
+      }
+    })
+    numbersMessaged.forEach(number => {
+      if (messageThreads[number]) {
+        messageThreads[number] = messageThreads.get(number).concat(messages.filter(message => message.from == number)).sort((messageA, messageB) => (messageA.dateSent > messageB.dateSent) ? 1 : -1);
+      } else {
+        messageThreads[number] = messages.filter(message => message.from == number || message.to == number).sort((messageA, messageB) => (messageA.dateSent > messageB.dateSent) ? 1 : -1);
+      }
+    })
+
+    resp['remainingMessages'] = messageThreads;
     resp['conversations'] = messageRepliedThreads;
     res.send(resp);
   });
